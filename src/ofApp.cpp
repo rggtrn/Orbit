22642c5a14304494bbf26839a0cd859a4583c855
Malandrone <emilio.ocelotl@gmail.com>
2018-11-04 23:09:56 -0600

    compatible con orb en sc

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -84,8 +84,8 @@ void ofApp::setup(){
     //image.loadImage("img/algorave16.png");
     //image.resize(100, 100);
     //mesh.setMode(OF_PRIMITIVE_POINTS);
-    glPointSize(2);
-    ofSetLineWidth(0.5);
+    glPointSize(1);
+    ofSetLineWidth(1);
     intensityThreshold = 100;
     timeScale = 1;
     int numVerts = mesh.getNumVertices();
@@ -510,6 +510,10 @@ void ofApp::update(){
         if (m.getAddress() == "/lago" && m.getNumArgs() == 1){
             lago = m.getArgAsFloat(0)+0.1;
         }
+
+        if (m.getAddress() == "/pointsize" && m.getNumArgs() == 1){
+            glPointSize(m.getArgAsFloat(0));
+        }
         
         if (m.getAddress() == "/mload" && m.getNumArgs() == 3){ /// modificar sintaxis
             string temp = "3d/" + m.getArgAsString(2) + ".obj";
@@ -673,6 +677,79 @@ void ofApp::update(){
         if (m.getAddress() == "/fb" && m.getNumArgs() == 1){
             feedbackON = m.getArgAsInt(0);
             depth = m.getArgAsInt(0);
+        }
+
+	if (m.getAddress() == "/mesh" && m.getNumArgs() == 3){
+            mesh.clear();
+            mechON = 1;
+            image.loadImage("img/"+ m.getArgAsString(1));
+            image.update();
+            image.resize(100, 100);
+            intensityThreshold = m.getArgAsFloat(0);
+            int w = image.getWidth();
+            int h = image.getHeight();
+            for (int x=0; x<w; ++x) {
+                for (int y=0; y<h; ++y) {
+                    ofColor c = image.getColor(x, y);
+                    float intensity = c.getLightness();
+                    if (intensity >= intensityThreshold) {
+                        float saturation = c.getSaturation();
+                        float z = ofMap(saturation, 0, 255, -100, 100);
+                        ofVec3f pos(x * 4, y * 4, z);
+                        mesh.addVertex(pos);
+                        mesh.addColor(c);
+                        offsets.push_back(ofVec3f(ofRandom(0, 100000), ofRandom(0,100000), ofRandom(0,100000)));
+                    }
+                }
+            }
+            
+            float connectionDistance = 4;
+            connectionDistance = m.getArgAsFloat(2);
+            
+            int numVerts = mesh.getNumVertices();
+            for (int a=0; a<numVerts; ++a) {
+                ofVec3f verta = mesh.getVertex(a);
+                for (int b=a+1; b<numVerts; ++b) {
+                    ofVec3f vertb = mesh.getVertex(b);
+                    float distance = verta.distance(vertb);
+                    if (distance <= connectionDistance) {
+                        mesh.addIndex(a);
+                        mesh.addIndex(b);
+                    }
+                }
+            }
+        }
+
+	if (m.getAddress() == "/meshscale"){
+	  meshscale = m.getArgAsFloat(0);
+	}
+
+	if (m.getAddress() == "/meshclear"){
+	  mesh.clear();
+	}
+
+	if (m.getAddress() == "/meshpos" && m.getNumArgs() == 3){
+	  meshPosX = m.getArgAsFloat(0);
+	  meshPosY = m.getArgAsFloat(1);
+	  meshPosZ = m.getArgAsFloat(2);
+	}
+
+	if (m.getAddress() == "/meshrot" && m.getNumArgs() == 3){
+	  meshRotX = m.getArgAsFloat(0);
+	  meshRotY = m.getArgAsFloat(1);
+	  meshRotZ = m.getArgAsFloat(2);
+	}
+
+        if (m.getAddress() == "meshmode" && m.getNumArgs() == 1){
+            if(m.getArgAsString(0) == "points"){
+            mesh.setMode(OF_PRIMITIVE_POINTS);
+            }
+            if(m.getArgAsString(0) == "lines"){
+            mesh.setMode(OF_PRIMITIVE_LINES);
+            }
+            if(m.getArgAsString(0) == "triangles"){
+            mesh.setMode(OF_PRIMITIVE_TRIANGLES);
+            }
         }
         
         if (m.getAddress() == "/cglitch" && m.getNumArgs() == 2){
